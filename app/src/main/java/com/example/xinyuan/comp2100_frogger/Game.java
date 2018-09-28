47d80ad6cb7e9dfa0d321df607dc02692d09438a
Dong  Xi <u6361000@anu.edu.au>
2018-09-29 00:18:41 +1000

    add wood

--- app/src/main/java/com/example/xinyuan/comp2100_frogger/Game.java
+++ app/src/main/java/com/example/xinyuan/comp2100_frogger/Game.java
@@ -6,13 +6,33 @@ import android.graphics.Paint;
 public class Game {
 
     private Frog frog;
+    private Car car;
 
     public Game() {
         frog = new Frog();
+        car = new Car(0,0);
     }
 
     // draw all the game
     public void draw(Canvas canvas, Paint paint) {
         frog.draw(canvas, paint);
+        car.draw(canvas,paint);
+
+    }
+
+    public void touch(String move) {
+        if (move == "GOUP") {
+            frog.pos.y -= 150;
+        }
+        else if (move == "GODOWN") {
+            frog.pos.y += 150;
+        }
+        else if (move == "GOLEFT") {
+            frog.pos.x -= 100;
+        }
+        else {
+            frog.pos.x += 100;
+        }
     }
 }
+
--- app/src/main/java/com/example/xinyuan/comp2100_frogger/RoadView.java
+++ app/src/main/java/com/example/xinyuan/comp2100_frogger/RoadView.java
@@ -5,28 +5,80 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class RoadView extends View {
+public class RoadView extends View implements View.OnTouchListener, Runnable {
 
+    float canvasH, canvasW;
     Paint paint;
     Game game;
 
     public RoadView(Context context,AttributeSet attrs) {
         super(context, attrs);
+
         paint = new Paint();
         game = new Game();
+        this.setOnTouchListener(this);
 
     }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvasH = canvas.getHeight();
+        canvasW = canvas.getWidth();
+        System.out.println("Height is " + canvasH);
+        System.out.println("2/3 Height is " + (2 * canvasH / 3));
+
         Paint paintBG = new Paint();
         paintBG.setColor(Color.BLUE);
-        paintBG.setStrokeWidth(600);
-        canvas.drawLine(0,500,canvas.getWidth(),500,paintBG);
+        paintBG.setStrokeWidth(450);
+        canvas.drawLine(0,525,canvas.getWidth(),525,paintBG);
+
+
+        // line showing UP DOWN LEFT RIGHT
+        paintBG.setStrokeWidth(10);
+        paintBG.setColor(Color.RED);
+        canvas.drawLine(0.5f* canvasW,canvasH / 3,0.5f * canvasW, (2 * canvasH / 3),paintBG);
+        canvas.drawLine(0,canvasH / 3,canvasW,canvasH/3,paintBG);
+        paintBG.setColor(Color.GREEN);
+        canvas.drawLine(0,((2 * canvasH / 3)),canvasW,((2 * canvasH / 3)),paintBG);
+
+
         game.draw(canvas,paint);
 
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        float userX = event.getX();
+        float userY = event.getY();
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            game.touch(checkRegion(userX,userY));
+        }
+        invalidate();
+        return true;
+    }
+
+    private String checkRegion(float x, float y) {
+        if (x <= canvasW && x >= 0 && y <= canvasH / 3) {
+            return "GOUP";
+        }
+        else if (x <= canvasW && x >= 0 && y >= 2 * canvasH / 3) {
+            return "GODOWN";
+        }
+        else if (x <= 0.5 * canvasW && x >= 0 && y < 2 * canvasH / 3 && y > canvasH / 3) {
+            return "GOLEFT";
+        }
+        else {
+            return "GORIGHT";
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
