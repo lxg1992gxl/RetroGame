fb07808e3ec1b10cf14dd2bb6a59a62af5018b2a
Xinyuan Ye <u6296255@anu.edu.au>
2018-09-28 23:48:35 +1000

    Frog is movable !!!

--- app/src/main/java/com/example/xinyuan/comp2100_frogger/Frog.java
+++ app/src/main/java/com/example/xinyuan/comp2100_frogger/Frog.java
@@ -18,8 +18,20 @@ public class Frog extends Sprite{
         int h = c.getHeight();
         int w = c.getWidth();
 
-        float xc = pos.x * w;
-        float yc = pos.y * h;
+        float xc, yc;
+
+        if (pos.x == 0.5f && pos.y == 0.9f) {
+            xc = pos.x * w;
+            yc = pos.y * h;
+            pos.x = xc;
+            pos.y = yc;
+            System.out.println("inital x " + xc + " and y is "+ yc);
+        }
+        else {
+            xc = pos.x;
+            yc = pos.y;
+        }
+
         p.setColor(Color.GREEN);
 
         c.drawCircle(xc,yc,20,p);
