0a56495daad61693d42bfd3e7cacd55cd731ebff
Xinyuan Ye <u6296255@anu.edu.au>
2018-09-28 20:55:39 +1000

    Initial commit

---
+++ app/src/main/java/com/example/xinyuan/comp2100_frogger/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.xinyuan.comp2100_frogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
