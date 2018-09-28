787b83bfc21d52fed081e6261b047245624e5a6f
Xinyuan Ye <u6296255@anu.edu.au>
2018-09-28 21:08:26 +1000

    Game activity added

---
+++ app/src/main/java/com/example/xinyuan/comp2100_frogger/RoadView.java
@@ -0,0 +1,25 @@
+package com.example.xinyuan.comp2100_frogger;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RoadView extends View {
+
+    public RoadView(Context context,AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paintBG = new Paint();
+        paintBG.setColor(Color.BLUE);
+        paintBG.setStrokeWidth(600);
+        canvas.drawLine(0,500,canvas.getWidth(),500,paintBG);
+    }
+
+}
--- app/src/main/java/com/example/xinyuan/comp2100_frogger/WelcomeActivity.java
+++ app/src/main/java/com/example/xinyuan/comp2100_frogger/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.xinyuan.comp2100_frogger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void startPressed(View view) {
+        Intent intent = new Intent(this,GameActivity.class);
+        startActivity(intent);
     }
 }
