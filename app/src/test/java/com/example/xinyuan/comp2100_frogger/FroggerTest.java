3f4f1013e511be59d0d2091fb636f51a7a0c8218
Xiaoguang Lyu <xiaoguang.lyu@anu.edu.au>
2018-10-18 16:00:22 +1100

    add test

--- app/src/test/java/com/example/xinyuan/comp2100_frogger/FroggerTest.java
+++ app/src/test/java/com/example/xinyuan/comp2100_frogger/FroggerTest.java
@@ -18,6 +18,14 @@ public class FroggerTest {
     public void testGenerateCars() {
         assertEquals(9, Cars.generateCar().size());
     }
-    
+
+    @Test
+    public void testRrplace() {
+        Frog a = new Frog();
+        a.pos.replace();
+        assertTrue("The x position of frog a should be 0.5, but get",a.pos.x==0.5f);
+        assertTrue("The y position of frog a should be 0.5, but get",a.pos.y==0.9f);
+    }
+
 
 }
